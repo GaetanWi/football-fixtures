ccfc583a371df3dc8518dc7650a4056c1a53f630
Gaetan Wichlacz <gaetan.wichlacz@gmail.com>
2020-02-19 00:26:09 +0100

    :sparkles: init matchcards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <div class="match-cards" v-for="fixture in dailyFeedData.response.g" :key="fixture.id">
+            <div class="match-card">
+                {{ fixture.s1 }} 
+                <span class="score">{{ fixture.sc1 }} - {{fixture.sc2}}</span> 
+                {{ fixture.s2 }}
+            </div>
+        </div>
         <pre>{{ dailyFeedData }}</pre>
     </div>
 </template>
@@ -27,7 +34,20 @@ export default class Home extends Vue {
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.match-cards {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
+    .match-card {
+        margin: 15px;
+        
+
+        .score {
+            color: #fa0020;
+        }
+    }
+}
 
 </style>
