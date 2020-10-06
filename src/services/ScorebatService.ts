9eae10cc1e79e3796c5f0850ece97f80238c1187
Gaetan Wichlacz <gaetan.wichlacz@gmail.com>
2020-10-06 14:59:09 +0200

    feat(): branchement pour deploiement static

--- src/services/ScorebatService.ts
+++ src/services/ScorebatService.ts
@@ -2,11 +2,13 @@ import axios, { AxiosPromise } from 'axios';
 
 export default class ScorebatService {
     
+    public static baseUrl = `https://www.scorebat.com/api`;
+
     public static getDailyFeed(): AxiosPromise<any> {
-        return axios.get('/api/feed');
+        return axios.get(`${this.baseUrl}/feed`);
     }
 
     public static getBoardInfo(idLeague: string): AxiosPromise<any> {
-        return axios.get(`/api/competition/${idLeague}`);
+        return axios.get(`${this.baseUrl}/competition/${idLeague}`);
     }
 }
