62dcc5d24d75059be736a8ed0a371c59bbe8ef73
Gaetan <gaetan.wichlacz@gmail.com>
2020-10-09 23:55:01 +0200

    feat(): add match highlights

--- src/components/MatchCard/MatchCard.vue
+++ src/components/MatchCard/MatchCard.vue
@@ -14,8 +14,11 @@
                     </template>
                 </v-expansion-panel-header>
                 <v-expansion-panel-content>
-                    <!-- You could add whatever you want in here -->
-                    One day, I'll add something there. But not today!
+                    <div v-if="matchData" style="display: flex; flex-direction: row; justify-content: center;">
+                        <TeamLineUp :lineup="matchData.l1" :bench="matchData.b1" style="width:30%"/>
+                        <Events :events="matchData.ev" style="width: 30%;"/>
+                        <TeamLineUp :lineup="matchData.l2" :bench="matchData.b2" style="width:30%"/>
+                    </div>
                 </v-expansion-panel-content>
             </v-expansion-panel>
         </v-expansion-panels>
@@ -29,15 +32,28 @@ import { Prop } from "vue-property-decorator";
 
 import Team from "@/components/MatchCard/MatchCardContent/Team.vue";
 import ScorePlaceHolder from "@/components/MatchCard/MatchCardContent/ScorePlaceHolder.vue";
+import TeamLineUp from '@/components/MatchCard/MatchCardContent/TeamLineUp.vue';
+import Events from '@/components/MatchCard/MatchCardContent/Events.vue';
+
+import ScorebatService from '@/services/ScorebatService';
 
 @Component({
     components: {
         Team,
-        ScorePlaceHolder
+        ScorePlaceHolder,
+        TeamLineUp,
+        Events
     }
 })
 export default class MatchCard extends Vue {
     @Prop() fixture: any;
+
+    public matchData: any = {};
+
+    public mounted() {
+        ScorebatService.getMatchData(this.fixture.s1, this.fixture.s2)
+        .then((response: any) => this.matchData = response.data.response);
+    }
 }
 </script>
 
